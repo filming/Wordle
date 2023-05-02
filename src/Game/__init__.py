f47a9857ca6ba9450bd4df73d1ee0a373af47880
Filming <79931565+filming@users.noreply.github.com>
2023-05-02 13:39:04 -0400

    Add setup & new_game base code

--- src/Game/__init__.py
+++ src/Game/__init__.py
@@ -1,9 +1,31 @@
-
+from random import choice
 
 
 class Game():
     def __init__(self):
-        pass
+        self.wordlist = []
+        self.game_word = ""
+
+    def setup(self):
+        wordlist = []
+        with open("../../storage/wordlist.txt", encoding="utf-8") as f:
+            for line in f.readlines():
+                line = line.strip()
+                if line: wordlist.append(line)
+
+        self.wordlist = wordlist
+    
+    def new_game(self):
+        game_word = choice(self.wordlist)
+        self.game_word = game_word
+
+    
+
+
+        
+        
+        
+
 
 
 
