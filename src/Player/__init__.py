2c817024b9a16d3ad2a712cf85967a6cebcb1722
Filming <79931565+filming@users.noreply.github.com>
2023-05-02 19:00:41 -0400

    Add make guess method to Player class

--- src/Player/__init__.py
+++ src/Player/__init__.py
@@ -1,5 +1,11 @@
 
 
 class Player():
-    def __init__(self):
+    def __init__(self, initGame):
+        self.game = initGame
+        self.remaining_guesses = 6
+        self.player_guesses = []
+    
+    def make_guess(self, guess):
         pass
+
